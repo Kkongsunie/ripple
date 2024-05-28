0aee8859549db24623d777a46a0a45d5bef8dfe0
Prince Jules Correa <jrincepules@gmail.com>
2024-05-28 17:39:48 +0800

    fadf

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={clsx(quicksand.className, "w-screens h-screen")}>
+        <link rel="icon" href="/images/icon.ico" sizes="any" />
         <Toaster position="top-center" />
         {children}
       </body>
