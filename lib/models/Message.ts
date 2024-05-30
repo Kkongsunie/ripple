6b356f5b94e651e6e4e7396819f1ec1ba4cca4f2
Prince Jules Correa <jrincepules@gmail.com>
2024-05-31 00:20:19 +0800

    timestamps

--- lib/models/Message.ts
+++ lib/models/Message.ts
@@ -9,36 +9,39 @@ export interface MessageDocument extends Document {
   year: string;
 }
 
-const MessageSchema = new mongoose.Schema({
-  salutation: {
-    type: String,
-    required: true,
+const MessageSchema = new mongoose.Schema(
+  {
+    salutation: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    month: {
+      type: String,
+      required: true,
+    },
+    day: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
   },
-  message: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  month: {
-    type: String,
-    required: true,
-  },
-  day: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true },
+);
 
 const Message: Model<MessageDocument> =
   mongoose.models.Message ||
